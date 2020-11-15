675ba3066f67ef191e66ba511b01e92da5d5b815
Andrew Rutter <71574118+andlrutt@users.noreply.github.com>
2020-11-14 20:59:09 -0500

    Created Background info session

---
+++ andrew-s-portfolio/src/Background/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div className="wrapper">
+            <div className="about">
+            <h1>Andrew Rutter</h1>
+            <p>about me</p>
+            </div>
+        </div>
+
+        
+    );
+}
+
+export default About;
---
+++ andrew-s-portfolio/src/Background/Cover-image.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CoverImage() {
+    return (
+        <div className="cover-image">
+            <img src="coverimage.png" alt=""></img>           
+        </div>
+
+    );
+}
+
+export default CoverImage;
