3e4b15b553c9c748b83343475ed523b1ab5a8779
Andrew Rutter <71574118+andlrutt@users.noreply.github.com>
2020-11-22 13:55:35 -0600

    Updated social links

--- andrew-s-portfolio/src/Contact.js
+++ andrew-s-portfolio/src/Contact.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import  ReactDOM  from  'react-dom';
 import {SocialIcon} from 'react-social-icons';
 
 
 function Contact() {
     return (
-        <div className="contact">
+        <footer className="contact" id="contact">
             <div className="social-icons">
-                <SocialIcon url="https://www.instagram.com/" />
-                <SocialIcon url="https://github.com/" />
+                <SocialIcon url="https://www.instagram.com/andyrewtn/" target="_blank"/>
+                <SocialIcon url="https://github.com/andlrutt?tab=repositories" target="_blank"/>
             </div>
             <div className="contact-info">
                 <a href ="mailto:andrewlorutter@gmail.com">Email</a>
                 <a href="tel:931-494-3566">931-494-3566</a>
             </div>
-        </div>
+        </footer>
     );
 }
 
