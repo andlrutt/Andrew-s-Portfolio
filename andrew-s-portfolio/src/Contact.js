882276bef4c646a6129ebd5ed42ffd921cf25aed
Andrew Rutter <71574118+andlrutt@users.noreply.github.com>
2020-11-21 21:04:47 -0600

    Added outline for contact info

--- andrew-s-portfolio/src/Contact.js
+++ andrew-s-portfolio/src/Contact.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import  ReactDOM  from  'react-dom';
+import {SocialIcon} from 'react-social-icons';
+
 
 function Contact() {
     return (
         <div className="contact">
-            <p>Outline for contact info. Have logos for media as well as email/phone</p>
+            <div className="social-icons">
+                <SocialIcon url="https://www.instagram.com/" />
+                <SocialIcon url="https://github.com/" />
+            </div>
+            <div className="contact-info">
+                <a href ="mailto:andrewlorutter@gmail.com">Email</a>
+                <a href="tel:931-494-3566">931-494-3566</a>
+            </div>
         </div>
     );
 }
