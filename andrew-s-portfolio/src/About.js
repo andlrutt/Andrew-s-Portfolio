389854dab5b8201f6ec2acd81ffd63f6823b992b
Andrew Rutter <71574118+andlrutt@users.noreply.github.com>
2020-11-27 12:57:00 -0600

    Removed sub-components from About component

--- andrew-s-portfolio/src/About.js
+++ andrew-s-portfolio/src/About.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import AboutTitle from './AboutTitle';
-import AboutContent from './AboutContent';
 
 function About() {
     return (
         
         <div className="about">
+            <h2>All about me!</h2>
+            <p>Hi, I'm Andrew! I'm a student at the University of Tennessee Knoxville who loves all things computers and code. I am currently based in Knoxville, TN. In my spare time, I like to work on small personal electronic or coding projects, or explore the great outdoors.</p>
             <a class="anchor" id="about" href="anchor">anchor</a>
-            <AboutTitle />
-            <AboutContent />
         </div>
 
     );
